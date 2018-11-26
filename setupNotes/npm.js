6aee67042a5149015e1543c03c9dc355a01567e2
Jeffery Patton <jefferypatton@jeffpattonsmbp.lan>
2018-11-26 12:27:24 -0500

    npm notes

---
+++ setupNotes/npm.js
@@ -0,0 +1,20 @@
+/*
+  NPM - Node Package Manager
+    Bundled with Node - See video on Node creator's thoughts on it now.
+
+    Common Commands(start with npm first):
+      init - Stands for initialize: start a new project using NPM, include -y
+      install - Install a package(you can shorten to i) OR re-install all dependencies for a package that has lost its node_modules. You can install as many packages at a time as you please.
+
+    package.json:
+      - Keeps track of high level information about your project.
+      - Also used for keeping track of all library/package dependencies required for your project to run.
+      - As well as common scripts you will use with your application
+      
+    package-lock.json
+      - keeps track of the version of the code you installed the first time you installed it.
+      - This is to ensure that when packages are updated it does not unintentionally break your code
+
+
+
+*/
