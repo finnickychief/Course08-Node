9df08ed5c9326c3c8d2c07d75d5857b6b1860973
Jeffery Patton <jefferypatton@jeffpattonsmbp.lan>
2018-12-05 11:48:01 -0500

    teams fin and momentjs

--- node-day7/models/Team.js
+++ node-day7/models/Team.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
+// npm i moment - Momentjs.com for info
+const moment = require('moment');
 
 const TeamSchema = new mongoose.Schema({
   name: { type: String, required: true, default: '' },
   division: { type: String, required: true, default: 'Unorganized' },
   hometown: { type: String, required: true },
   mascot: { type: String },
-  colors: { type: Array }
+  colors: { type: Array },
+  createdTimeStamp: {
+    type: String,
+    // 'dddd, MMMM Do YYYY, h:mm:ss a'
+    // 4d's - Full day(Wednesday), 4 M's full month(December), Do Date in month(5th),  4 Ys is full year (2018), h:mm:ss is clock time, a means am/pm
+    default: moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
+  }
 });
 
 module.exports = mongoose.model('teams', TeamSchema);
