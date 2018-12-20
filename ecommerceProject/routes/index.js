9a8503e170a17269132ef94812bff6465e20d7a4
Jeff Patton <jtpatton92@gmail.com>
2018-12-20 13:41:22 -0500

    stripe checkout fin-ished

--- ecommerceProject/routes/index.js
+++ ecommerceProject/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { authLocal, authJWT, generateToken } = require('../middleware/auth');
 const ProductController = require('../controllers/ProductController');
+var stripe = require('stripe')('sk_test_rcuW40YCvnAydOivyKCoruoR');
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -17,8 +18,33 @@ router.get('/', (req, res) => {
 });
 
 router.post('/checkout', (req, res) => {
+  // var stripe = require('stripe')('sk_test_rcuW40YCvnAydOivyKCoruoR'); // Put at top of file
   console.log('in checkout');
-  res.json({ message: '?' });
+  console.log(req.body);
+  const token = req.body.token;
+  console.log(token);
+
+  const totalPrice = req.body.cart.reduce((acc, item) => {
+    return acc + item.price * item.count;
+  }, 0);
+
+  console.log(totalPrice);
+
+  const charge = stripe.charges.create({
+    amount: totalPrice,
+    currency: 'usd',
+    description: 'Example charge',
+    source: 'tok_visa'
+  });
+  charge
+    .then(resp => {
+      console.log('in good charge');
+      console.log(resp);
+    })
+    .catch(err => {
+      console.log('in bad charge');
+      console.log(err);
+    });
 });
 
 router.get('/cart', (req, res) => {
