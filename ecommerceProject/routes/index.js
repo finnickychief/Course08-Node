071faa5710b1e8f9d7e74bd1a3864a146c0e9dbe
Jeff Patton <jtpatton92@gmail.com>
2018-12-20 12:02:22 -0500

    cart

--- ecommerceProject/routes/index.js
+++ ecommerceProject/routes/index.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { authLocal, authJWT, generateToken } = require('../middleware/auth');
+const ProductController = require('../controllers/ProductController');
 
 /* GET home page. */
-router.get('/', authJWT(), (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  ProductController.getProducts()
+    .then(prodData => {
+      res.render('products', { title: 'Product Page', prodData: prodData });
+    })
+    .catch(err =>
+      res
+        .status(404)
+        .render('error', { message: 'Issue retrieving products', err })
+    );
 });
 
-router.post('/', (req, res) => {
-  res.render('index', { title: req.body.username });
-});
-
-router.get('/products', (req, res) => {
-  let prodData=[
-    {prodName:'Prod 1',prodDesc:'Prod 1 Desc', prodPrice:100, prodImg:'prod_1.JPG'},
-    {prodName:'Prod 2',prodDesc:'Prod 2 Desc', prodPrice:110, prodImg:'prod_2.jpg'},
-    {prodName:'Prod 3',prodDesc:'Prod 3 Desc', prodPrice:120, prodImg:'prod_3.jpg'},
-    {prodName:'Prod 4',prodDesc:'Prod 4 Desc', prodPrice:100, prodImg:'prod_4.jpg'},
-    {prodName:'Prod 5',prodDesc:'Prod 5 Desc', prodPrice:140, prodImg:'prod_5.jpg'},
-    {prodName:'Prod 6',prodDesc:'Prod 6 Desc', prodPrice:140, prodImg:'prod_6.jpg'},
-    {prodName:'Prod 7',prodDesc:'Prod 7 Desc', prodPrice:100, prodImg:'prod_7.jpg'},
-    {prodName:'Prod 8',prodDesc:'Prod 8 Desc', prodPrice:120, prodImg:'prod_8.jpg'},
-    {prodName:'Prod 9',prodDesc:'Prod 9 Desc', prodPrice:125, prodImg:'prod_9.jpg'}
-  ]
-  res.render('products', { title: 'Product Page',prodData: prodData});
+router.get('/cart', (req, res) => {
+  res.render('cart');
 });
 
 router.get('/about', (req, res) => {
